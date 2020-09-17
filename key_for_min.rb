8a58e96792d6694224373ba4d627620ec24bf7d7
Latisha Joe <latisha_joe@yahoo.com>
2020-09-17 14:04:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
    smallest_key = nil
-   smallest_value = nil
+   smallest_value = 0
    name_hash.each do |key, value|
      if smallest_value == nil || value < smallest_value
        smallest_value = value
